f69555dde21df9ea786634d589238e576704915b
Sanchali-rif <sanchalisaha05@gmail.com>
2026-09-20 23:55:48 +0530

    added controller into routes

--- Backend/src/controllers/ai.constroller.js
+++ Backend/src/controllers/ai.constroller.js
@@ -1,6 +1,6 @@
 const aiService=require("../services/ai.service")
 
-module.export=async (req, res) => {
+module.export.getResponse=async (req, res) => {
     const prompt=req.query.prompt;
     if(!prompt){
         return res.status(400).send("prompt is required");
--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const aiController = require("../controllers/ai.constroller");
 
-router.get("/get-response", )
+router.get("/get-response", aiController.getResponse);
 
 module.exports = router;
