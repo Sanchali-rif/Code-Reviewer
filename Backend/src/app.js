bad2a3085a467ff3005a7d6ed3088053f6af8d8e
Sanchali-rif <sanchalisaha05@gmail.com>
2026-09-21 23:10:59 +0530

    refactoring ai response handler

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const aiRoutes = require("../src/routes/ai.routes");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
