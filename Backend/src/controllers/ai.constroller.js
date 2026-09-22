7b7449b69db52d5b2f2114098ff547f93cc9e97d
Sanchali-rif <sanchalisaha05@gmail.com>
2026-09-22 13:58:38 +0530

    error handeled

--- Backend/src/controllers/ai.constroller.js
+++ Backend/src/controllers/ai.constroller.js
@@ -1,13 +1,13 @@
 const aiService = require("../services/ai.service");
 
 module.exports.getReview = async (req, res) => {
-    const prompt = req.body.prompt;
+    const code = req.body.code;
 
-    if (!prompt) {
-        return res.status(400).send("prompt is required");
+    if (!code) {
+        return res.status(400).send("code is required");
     }
 
-    const response = await aiService(prompt);
+    const response = await aiService(code);
 
     res.send(response);
 };
